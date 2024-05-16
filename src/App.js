f0eff8ae7d724af2690b48ca762920902d6c9891
Jean Piffaut <40282095+JeanPiffaut@users.noreply.github.com>
2024-05-16 17:35:16 -0400

    header estructura

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 import './styles/App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./views/Home";
--- src/views/Header.js
+++ src/views/Header.js
@@ -1,48 +1,77 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 function Header() {
     return (
         <header id="facto_header" className="navigation position-absolute w-100">
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
+            <nav className="bg-dark bg-opacity-50 navbar navbar-dark navbar-expand-lg">
+                <div className="container">
+                    <Link className="navbar-brand" to={"./#"}>Navbar</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                             data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                             aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Link</a>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                   data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a className="dropdown-item" href="#">Action</a></li>
-                                    <li><a className="dropdown-item" href="#">Another action</a></li>
-                                    <li>
-                                        <hr className="dropdown-divider"/>
+                    <div className={"collapse navbar-collapse"} id="navbarSupportedContent">
+                        <div className={"w-100"}>
+                            <div className={"d-flex"}>
+                                <ul className="navbar-nav ms-auto me-0 mb-2 mb-lg-0">
+                                    <li className="nav-item">
+                                        <a className="nav-link active" aria-current="page" href="#">Home</a>
                                     </li>
-                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="./#">Link</a>
+                                    </li>
+                                    <li className="nav-item dropdown">
+                                        <button className="nav-link dropdown-toggle" id="navbarDropdown" role="button"
+                                                data-bs-toggle="dropdown" aria-expanded="false">
+                                            Dropdown
+                                        </button>
+                                        <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                            <li><a className="dropdown-item" href="./#">Action</a></li>
+                                            <li><a className="dropdown-item" href="./#">Another action</a></li>
+                                            <li>
+                                                <hr className="dropdown-divider"/>
+                                            </li>
+                                            <li><a className="dropdown-item" href="./#">Something else here</a></li>
+                                        </ul>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link disabled" href="./#" tabIndex="-1"
+                                           aria-disabled="true">Disabled</a>
+                                    </li>
                                 </ul>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link disabled" href="#" tabIndex="-1"
-                                   aria-disabled="true">Disabled</a>
-                            </li>
-                        </ul>
-                        <form className="d-flex">
-                            <input className="form-control me-2" type="search" placeholder="Search"
-                                   aria-label="Search"/>
-                            <button className="btn btn-outline-success" type="submit">Search</button>
-                        </form>
+                            </div>
+                            <hr className={"my-0"}/>
+                            <div className={"d-flex"}>
+                                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                    <li className="nav-item">
+                                        <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="./#">Link</a>
+                                    </li>
+                                    <li className="nav-item dropdown">
+                                        <button className="nav-link dropdown-toggle" id="navbarDropdown" role="button"
+                                                data-bs-toggle="dropdown" aria-expanded="false">
+                                            Dropdown
+                                        </button>
+                                        <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                            <li><a className="dropdown-item" href="./#">Action</a></li>
+                                            <li><a className="dropdown-item" href="./#">Another action</a></li>
+                                            <li>
+                                                <hr className="dropdown-divider"/>
+                                            </li>
+                                            <li><a className="dropdown-item" href="./#">Something else here</a></li>
+                                        </ul>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link disabled" href="./#" tabIndex="-1"
+                                           aria-disabled="true">Disabled</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </nav>
